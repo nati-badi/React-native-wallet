1449f678d7b21b2cb1033e6656e9854e51d4fe1f
Nati <natibadigojimma@gmail.com>
2025-08-02 23:18:44 +0300

    feat(header): added a header for the home page

--- Mobile/app/(root)/index.jsx
+++ Mobile/app/(root)/index.jsx
@@ -1,40 +1,59 @@
-import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { useUser } from "@clerk/clerk-expo";
+import { useRouter } from "expo-router";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SignOutButton } from "@/components/SignOutButton";
 import { useTransactions } from "../../hooks/useTransactions.js";
 import { useEffect } from "react";
+import PageLoader from "../../components/PageLoader.jsx";
+import { styles } from "../../assets/styles/home.styles.js";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Page() {
   const { user } = useUser();
   const { transactions, summary, isLoading, loadData, deleteTransaction } =
     useTransactions(user.id);
 
+  const router = useRouter();
+
   useEffect(() => {
     loadData();
   }, [loadData]);
 
-  console.log("transactions: ", transactions);
-  console.log("summary: ", summary);
-  console.log(user.id);
+  if (isLoading) return <PageLoader />;
 
   return (
-    <View>
-      <SignedIn>
-        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-        <Text>Balance: {summary.summary?.balance}</Text>
-        <Text>Income: {summary.summary?.income}</Text>
-        <Text>Expense: {summary.summary?.expense}</Text>
-        <SignOutButton />
-      </SignedIn>
-      <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text>Sign in</Text>
-        </Link>
-        <Link href="/(auth)/sign-up">
-          <Text>Sign up</Text>
-        </Link>
-      </SignedOut>
+    <View style={styles.container}>
+      <View style={styles.content}>
+        {/* HEADER */}
+        <View style={styles.header}>
+          {/* LEFT HEADER */}
+          <View style={styles.headerLeft}>
+            <Image
+              source={require("../../assets/images/logo.png")}
+              style={styles.headerLogo}
+              resizeMode="contain"
+            ></Image>
+
+            <View style={styles.welcomeContainer}>
+              <Text style={styles.welcomeText}>Welcome</Text>
+              <Text style={styles.usernameText}>
+                {user?.emailAddresses[0]?.emailAddress.split("@")[0]}
+              </Text>
+            </View>
+          </View>
+          {/* RIGHT HEADER */}
+          <View style={styles.headerRight}>
+            <TouchableOpacity
+              style={styles.addButton}
+              onPress={() => router.push("/create")}
+            >
+              <Ionicons name="add" size={20} color="#FFF" />
+              <Text style={styles.addButtonText}>Add</Text>
+            </TouchableOpacity>
+            <SignOutButton />
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
---
+++ Mobile/components/PageLoader.jsx
@@ -0,0 +1,13 @@
+import { COLORS } from "../constants/colors.js";
+import { View, ActivityIndicator } from "react-native";
+import { styles } from "../assets/styles/home.styles.js";
+
+const PageLoader = () => {
+  return (
+    <View style={styles.loadingContainer}>
+      <ActivityIndicator size="large" color={COLORS.primary} />
+    </View>
+  );
+};
+
+export default PageLoader;
